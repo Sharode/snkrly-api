c249336e368f277a233a40a51629be7140dcce25
Neil Williams <Neil.Williams227@gmail.com>
2019-09-01 14:01:02 +0700

    preparing for deployment

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const path = require('path ')
 const connectDB = require('./config/db')
 
 //import Routes
@@ -25,13 +26,17 @@ app.use('/api/test/snkrs', testsnkrs)
 app.use('/api/test/brandlist', testbrandList)
 
 
-app.get('/', (req, res) => {
-    res.send('Home page')
-})
+// serve static assets in production
+ if (process.env.NODE_ENV === 'production') {
+      // set static folder
+      app.use(express.static('client/build '))
+
+      app.get("*", (req,res)=> {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+      })
+ }
 
 
-
-
-const PORT = process.env.port || 5000
+const PORT = process.env.PORT|| 5000
 
 app.listen(PORT, () => `listening on port ${5000}`)
