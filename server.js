4d55ff4eafde71a64e578a9db6e82309e3fe46ff
Neil Williams <Neil.Williams227@gmail.com>
2019-09-02 15:27:50 +0700

    server changes

--- server.js
+++ server.js
@@ -33,11 +33,8 @@ if (process.env.NODE_ENV === 'production') {
   // set static folder
   app.use(express.static('client/build '))
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-  })
 }
 
-const port = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000
 
-app.listen(port, () => console.log(`listening on port ${port}`))
+app.listen(PORT, () => console.log(`listening on port ${port}`))
